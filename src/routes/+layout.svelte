15a7822d852ac9c6f22b64af5416e9f92cd1d0c8
Andy Tyler <andrewjtyler1@gmail.com>
2026-02-25 12:30:44 +0000

    removed demo night stuff and added new dev mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,7 @@
 <script lang="ts">
-	import SEO from '$lib/utils/seo/SEO.svelte';
 	import '../app.css';
-	import type { LayoutServerData } from './$types';
 
-	let { data, children }: { data: LayoutServerData; children: any } = $props();
+	let { children }: { children: any } = $props();
 </script>
 
-<SEO />
-
-<main class="relative">
-	<div
-		class="absolute inset-0 z-0 h-screen w-screen bg-linear-180 from-transparent to-orange-500"
-	></div>
-
-	{@render children()}
-</main>
-
-<!-- From svelte docs
- 
-import * as db from '$lib/server/database';
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async () => {
-	return {
-		posts: await db.getPostSummaries()
-	};
-};
-
--->
+{@render children()}
--- src/routes/testing/+page.ts
+++
@@ -1,7 +0,0 @@
-export const load = async () => {
-	return {
-		seo: {
-			title: 'Testing - AJT.dev'
-		}
-	};
-};
